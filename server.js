ce7dfd78df7cffc44f8cc6347f6bfa69df233d60
Gagan Singh <gagansingh.dev@outlook.com>
2020-09-30 18:37:16 -0400

    Added file based data store and implemented env variables

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+
+const STORE = require("./data/STORE");
+
+// INITIALIZING APP VARIABLE
 const app = express();
 
-app.use(morgan("dev"));
+// MIDDLEWARES
+app.use(morgan("dev")); // Basic logger
+app.use(cors());
 
+// Generic Route
 app.get("/", (req, res) => res.json({ message: "Hello World!" }));
 
-const PORT = process.env.PORT || 4000;
+app.get("/api/products", (req, res) => res.json(STORE));
+
+app.get("/api/products/:id", (req, res) => {
+    const product = STORE.find((p) => p._id === req.params.id);
+    res.json(product);
+});
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
-    console.log(`Server running at http://localhost:${PORT}`)
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`
+    )
 );
