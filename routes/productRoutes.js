6889f9a4f21d4ad3d88b7f1799f394949dc81c12
Gagan Singh <gagansingh.dev@outlook.com>
2020-10-01 19:21:30 -0400

    Cleaned up code

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,31 +6,27 @@ import Product from "../models/productModel.js";
 
 // GET      /api/products
 //          Public route
-// prettier-ignore
-router
-    .route("/")
-    .get(
-        asyncHandler(async (req, res) => {
-            const products = await Product.find({});
-            res.json(products);
-        })
-    );
+router.route("/").get(
+    asyncHandler(async (req, res) => {
+        const products = await Product.find({});
+        res.json(products);
+    })
+);
 
 // GET      /api/products/:id
 //          Public route
-// prettier-ignore
-router
-    .route("/:id")
-    .get(
-        asyncHandler(async (req, res) => {
-            const product = await Product.findById(req.params.id);
-            if (product) {
-                res.json(product);
-            } else {
-                res.status(404);
-                throw new Error(`Product with id ${req.params.id} does not exist on the server.`);
-            }
-        })
-    );
+router.route("/:id").get(
+    asyncHandler(async (req, res) => {
+        const product = await Product.findById(req.params.id);
+        if (product) {
+            res.json(product);
+        } else {
+            res.status(404);
+            throw new Error(
+                `Product with id ${req.params.id} does not exist on the server.`
+            );
+        }
+    })
+);
 
 export default router;
