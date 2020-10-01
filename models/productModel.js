211c947182d2694101aea72ffff88751485871b7
Gagan Singh <gagansingh.dev@outlook.com>
2020-09-30 21:16:58 -0400

    Implemented db schemas

---
+++ models/productModel.js
@@ -0,0 +1,81 @@
+import mongoose from "mongoose";
+
+// schema for a single review
+const reviewSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        rating: {
+            type: Number,
+            required: true,
+        },
+        comment: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const productSchema = mongoose.Schema(
+    {
+        user: {
+            // Create a reference to the userSchema
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "User", // Which schema to refer to?
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        brand: {
+            type: String,
+            required: true,
+        },
+        category: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        reviews: [reviewSchema], // Array/collection of reviews
+        rating: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        numReviews: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        price: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        countInStock: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
