4d2790db1f0a346097c7cc0398dc282160b5fcc1
Gagan Singh <gagansingh.dev@outlook.com>
2020-10-02 21:51:54 -0400

    Created auth middleware and protected the user profile route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 import asyncHandler from "express-async-handler";
+
 import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 const authUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
@@ -11,7 +13,7 @@ const authUser = asyncHandler(async (req, res) => {
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: "TODO: ADD TOKEN",
+            token: generateToken(user._id),
         });
     } else {
         res.status(401);
@@ -19,4 +21,20 @@ const authUser = asyncHandler(async (req, res) => {
     }
 });
 
-export { authUser };
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } else {
+        res.status(401);
+        throw new Error("User not found");
+    }
+});
+
+export { authUser, getUserProfile };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
 const router = express.Router();
 
-import { authUser } from "../controllers/userController.js";
+import { authUser, getUserProfile } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 // POST     /api/users/login
 //          Public route
 router.route("/login").post(authUser);
+
+// GET      /api/users/profile
+//          Private route
+router.route("/profile").get(protect, getUserProfile);
 
 export default router;
